7bba9c6709f7fe57afb65cc72b230b424f52e45e
Rahul Raghavendra <rahulraghavendra.raja@gmail.com>
2026-05-28 09:47:21 +0530

    Built portfolio management API foundation

--- backend/app/api/portfolio.py
+++ backend/app/api/portfolio.py
@@ -1,26 +1,57 @@
 from fastapi import APIRouter
+from fastapi import Depends
+
+from sqlalchemy.orm import Session
+
+from app.database.db import get_db
+
+from app.models.portfolio import Portfolio
+
+from app.schemas.portfolio_schema import (
+    PortfolioCreate
+)
 
 router = APIRouter(
     prefix="/portfolio",
     tags=["Portfolio"]
 )
 
-@router.get("/")
-def get_portfolio():
+# ADD STOCK
+@router.post("/add")
+def add_stock(
+    data:PortfolioCreate,
+    db:Session = Depends(get_db)
+):
+
+    portfolio = Portfolio(
+
+        symbol=data.symbol,
+
+        shares=data.shares,
+
+        average_price=data.average_price,
+
+        user_id=1
+    )
+
+    db.add(portfolio)
+
+    db.commit()
+
+    db.refresh(portfolio)
 
     return {
-
-        "portfolio":[
-
-            {
-                "symbol":"AAPL",
-                "shares":120
-            },
-
-            {
-                "symbol":"NVDA",
-                "shares":45
-            }
-
-        ]
+        "message":"Stock Added"
     }
+
+# GET PORTFOLIO
+@router.get("/")
+def get_portfolio(
+    db:Session = Depends(get_db)
+):
+
+    portfolio = db.query(
+        Portfolio
+    ).all()
+
+    return portfolio
